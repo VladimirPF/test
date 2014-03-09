16b13c328e15e26b497c466f63567648fa68ffa4
vladimir.perez <vladimir.perez@twipemobile.com>
2014-03-09 13:48:12 +0100

    playing with Git

--- TestConsole_Git/TestConsole_Git/Program.cs
+++ TestConsole_Git/TestConsole_Git/Program.cs
@@ -12,6 +12,20 @@ namespace TestConsole_Git
         {            
             // added 1 comment from visual studio git
             // added 2 comment git
+            Console.WriteLine("Calculator");
+        
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            double total = double.Parse(a) + double.Parse(b);
+
+            Console.WriteLine( "a + b ="  + total  );
+
+            Console.ReadLine();
+           
+
         }
+
+       
     }
 }
